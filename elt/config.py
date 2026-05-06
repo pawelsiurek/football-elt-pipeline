2e216f7e644c5737fe06f80c9561b0883dbfbbe9
Pawel Siurek <pawel.siurek77@gmail.com>
2026-05-06 16:05:31 +0200

    feat: add config and extract modules

---
+++ elt/config.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# API
+API_KEY = os.getenv('API_KEY')
+COMPETITION_CODE = os.getenv('COMPETITION_CODE')
+
+# PostgreSQL
+DB_NAME = os.getenv('DB_NAME')
+DB_PORT = os.getenv('DB_PORT')
+DB_USER = os.getenv('DB_USER')
+DB_HOST = os.getenv('DB_HOST')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+# Connection string
+DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
---
+++ elt/extract.py
@@ -0,0 +1,64 @@
+import requests
+from loguru import logger
+from config import API_KEY, COMPETITION_CODE
+
+BASE_URL = "https://api.football-data.org/v4"
+
+def extract_matches(competition_code: str) -> dict:
+    """Fetch all matches for a given competition from football-data.org"""
+    
+    url = f"{BASE_URL}/competitions/{competition_code}/matches"
+    headers = {"X-Auth-Token": API_KEY}
+    
+    logger.info(f"Extracting matches for competition: {competition_code}")
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code != 200:
+        raise Exception(f"API Error {response.status_code}: {response.text}")
+
+    data = response.json()
+    logger.info(f"Extracted {len(data['matches'])} matches.")
+
+    return data
+    
+def extract_standings(competition_code: str) -> dict:
+    """Fetch current standings for a given competition"""
+    
+    url = f"{BASE_URL}/competitions/{competition_code}/standings"
+    headers = {"X-Auth-Token": API_KEY}
+    
+    logger.info(f"Extracting standings for competition: {competition_code}")
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code != 200:
+        raise Exception(f"API Error {response.status_code}: {response.text}")
+    
+    data = response.json()
+    logger.info("Extracted standings successfully")
+    return data
+
+def extract_scorers(competition_code: str) -> dict:
+    """Fetch top scorers for a given competition"""
+    
+    url = f"{BASE_URL}/competitions/{competition_code}/scorers"
+    headers = {"X-Auth-Token": API_KEY}
+    
+    logger.info(f"Extracting top scorers for competition: {competition_code}")
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code != 200:
+        raise Exception(f"API Error {response.status_code}: {response.text}")
+
+    data = response.json()
+    logger.info(f"Extracted {len(data['scorers'])} scorers")
+    
+    return data
+
+if __name__ == "__main__":
+    matches = extract_matches(COMPETITION_CODE)
+    standings = extract_standings(COMPETITION_CODE)
+    scorers = extract_scorers(COMPETITION_CODE)
+    print(matches)
